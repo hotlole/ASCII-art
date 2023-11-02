3ec1c06dac4d3ef8308ba626c118ee65fd86c445
Данила Косторев <kostorev2004@mail.ru>
2023-11-03 03:59:52 +0700

    Добавьте файлы проекта.

---
+++ ASCII art/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace ASCII_art
+{
+    internal class Program
+    {
+        private const int MAX_WIDTH = 474;
+        private const double WIDTH_OFFSET = 1.7;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            
+
+
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images | *.bmp; *.png; *.jpg; *.JPEG" 
+            };
+
+            Console.WriteLine("Press enter to start...\n");
+
+            while (true)
+            {
+                Console.ReadLine();
+
+                if (openFileDialog.ShowDialog() != DialogResult.OK) 
+                    continue;
+                
+                Console.Clear();
+
+                var bitmap = new Bitmap(openFileDialog.FileName);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.ToGrayscale();
+
+                var converter = new BitmapToASCIIConverter(bitmap);
+                var rows = converter.Convert();
+
+                foreach ( var row in rows )
+                    Console.WriteLine(row);
+
+
+                var rowNegative = converter.ConvertNegative();
+                File.WriteAllLines("image.txt",rows.Select(r => new string(r)));
+                Console.SetCursorPosition(0,0);
+                
+
+                
+
+            }
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            
+            var newHeight = bitmap.Height / WIDTH_OFFSET * MAX_WIDTH / bitmap.Width;
+            if (bitmap.Width > MAX_WIDTH || bitmap.Height > newHeight)
+                bitmap = new Bitmap(bitmap, new Size(MAX_WIDTH, (int) newHeight));
+            return bitmap;
+            
+
+            
+        }
+    }
+}
